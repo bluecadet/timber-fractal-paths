12f18b33be9e0296edcbb5eb3a21cbf8f2cc8c7a
Baerkins <shaun.baer@gmail.com>
2022-10-25 15:33:19 -0400

    Support Twig 2.7+

---
+++ lib/v2/CustomChainLoader_v2.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace TimberFractalPathsChainLoader;
+
+use Twig_LoaderInterface;
+use Twig_ExistsLoaderInterface;
+
+
+/**
+ * Twigs Chain Loader, but modified for Timber, based of Drupal Twig_Loader_Chain class
+ *
+ * @link https://api.drupal.org/api/drupal/vendor%21twig%21twig%21lib%21Twig%21Loader%21Chain.php/class/Twig_Loader_Chain/8.2.x
+ *
+ */
+class FractalPathChainLoader implements Twig_LoaderInterface, Twig_ExistsLoaderInterface {
+  private $hasSourceCache = array();
+  protected $loaders = array();
+
+  /**
+   * Constructor.
+   *
+   * @param Twig_LoaderInterface[] $loaders An array of loader instances
+   */
+  public function __construct(array $loaders = array()) {
+    foreach ($loaders as $loader) {
+      $this->addLoader($loader);
+    }
+  }
+
+  /**
+   * Adds a loader instance.
+   *
+   * @param Twig_LoaderInterface $loader A Loader instance
+   */
+  public function addLoader(Twig_LoaderInterface $loader) {
+    $this->loaders[] = $loader;
+    $this->hasSourceCache = array();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSourceContext($name) {
+    $exceptions = array();
+    foreach ($this->loaders as $loader) {
+      if ($loader instanceof Twig_ExistsLoaderInterface && !$loader->exists($name)) {
+        continue;
+      }
+      try {
+        return $loader->getSourceContext($name);
+      } catch (Twig_Error_Loader $e) {
+        $exceptions[] = $e->getMessage();
+      }
+    }
+    throw new Twig_Error_Loader(sprintf('Template "%s" is not defined%s.', $name, $exceptions ? ' (' . implode(', ', $exceptions) . ')' : ''));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function exists($name) {
+    $name = (string) $name;
+    if (isset($this->hasSourceCache[$name])) {
+      return $this->hasSourceCache[$name];
+    }
+    foreach ($this->loaders as $loader) {
+      if ($loader instanceof Twig_ExistsLoaderInterface) {
+        if ($loader->exists($name)) {
+          return $this->hasSourceCache[$name] = true;
+        }
+        continue;
+      }
+      if ($loader->exists($name)) {
+        return $this->hasSourceCache[$name] = true;
+      }
+      // try {
+      //   $loader->getSourceContext($name);
+      //   return $this->hasSourceCache[$name] = true;
+      // } catch (Twig_Error_Loader $e) {
+      // }
+    }
+    return $this->hasSourceCache[$name] = false;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheKey($name) {
+    $exceptions = array();
+    foreach ($this->loaders as $loader) {
+      if ($loader instanceof Twig_ExistsLoaderInterface && !$loader->exists($name)) {
+        continue;
+      }
+      try {
+        return $loader->getCacheKey($name);
+      } catch (Twig_Error_Loader $e) {
+        $exceptions[] = get_class($loader) . ': ' . $e->getMessage();
+      }
+    }
+    throw new Twig_Error_Loader(sprintf('Template "%s" is not defined%s.', $name, $exceptions ? ' (' . implode(', ', $exceptions) . ')' : ''));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isFresh($name, $time) {
+    $exceptions = array();
+    foreach ($this->loaders as $loader) {
+      if ($loader instanceof Twig_ExistsLoaderInterface && !$loader->exists($name)) {
+        continue;
+      }
+      try {
+        return $loader->isFresh($name, $time);
+      } catch (Twig_Error_Loader $e) {
+        $exceptions[] = get_class($loader) . ': ' . $e->getMessage();
+      }
+    }
+    throw new Twig_Error_Loader(sprintf('Template "%s" is not defined%s.', $name, $exceptions ? ' (' . implode(', ', $exceptions) . ')' : ''));
+  }
+
+}
--- timber-fractal-paths.php
+++ timber-fractal-paths.php
@@ -8,8 +8,14 @@ Version: 1.0.0
 
 class TimberCustomLoader {
 
+  private $twig_version;
+  private $fractal_path;
+  private $fractal_handle;
+
   public function __construct() {
-    $this->version = '1.0.0';
+    $this->version        = '2.0.0';
+    $this->fractal_path   = defined('FRACTAL_HANDLE') ? FRACTAL_HANDLE : 'fractal';
+    $this->fractal_handle = defined('FRACTAL_PATH') ? FRACTAL_PATH : get_stylesheet_directory() . '/fractal/components';
 
     if (!is_admin()) {
       add_action('plugins_loaded', [$this, 'plugin_checks']);
@@ -21,12 +27,26 @@ class TimberCustomLoader {
   }
 
   public function add_loader($loader) {
-    require_once('lib/Loader.php');
-    require_once('lib/CustomChainLoader.php');
+
+    $this->twig_version = false;
+
+    if ( class_exists('\Twig\Environment') ) {
+      $this->twig_version =  Twig\Environment::VERSION;
+    } elseif ( class_exists('Twig_Environment') ) {
+      $this->twig_version = Twig_Environment::VERSION;
+    }
 
     // Add namespace to Timber Loader
-    $loader->addPath(FRACTAL_PATH, FRACTAL_HANDLE);
+    $loader->addPath($this->fractal_path, $this->fractal_handle);
     $load2 = $loader;
+
+    if ( version_compare($this->twig_version, '2.7.0', '>=') ) {
+      require_once('lib/v2/Loader_v2.php');
+      require_once('lib/v2/CustomChainLoader_v2.php');
+    } else {
+      require_once('lib/v1/Loader_v1.php');
+      require_once('lib/v1/CustomChainLoader_v1.php');
+    }
 
     // Create a custom loader to load Fractal style include:
     // `@patterns/component-name`, not `@patterns/component-name/component-name.twig`
